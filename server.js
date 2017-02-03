b68d283da51f578b8402367d76d14e923b632a43
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-03 17:46:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-one', function (req, res) {
 
 
 app.get('/article-two', function (req, res) {
-  res.send('Artc 2');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 
