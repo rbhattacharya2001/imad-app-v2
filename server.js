1b0fe631eab488439e7350daf434b42665744b80
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-07 20:26:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,13 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+var names=[];
+app.get('/submit-name/:name', function(req, res){
+    var name =req.params.name;
+    names.push(name);
+    res.send(JSON.stringyfy(names));
+    
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
