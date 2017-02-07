474a7a9cccd860e76ebd4afddae36d0481c8f488
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-07 17:56:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,15 +29,16 @@ button.onclick= function (){
     var name = nameInput.value;
     var submit= document.getElementById('submit_btn');
     submit.onclick=function(){
-        alert("I am an alert box!");
-         console.log('inside submit' );
+        
+         
         var names =['name1', 'name2', 'name3', 'name4'];
         var list='';
         for (var i=0;i<names.length;i++){
             list+='<li>'+names[i]+'</li>';
-           console.log('name ' +names[i] );
+          
         }
         console.log('after loop ' +list );
+        alert("I am an alert box!" + list);
        var ul=document.getElementById('nameList') ;
        ul.innerHTML=list;
     };  
