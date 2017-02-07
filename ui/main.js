8cb44b4887be95d047b74bcb41839d7cb9101561
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-07 21:32:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,17 +30,32 @@ button.onclick= function (){
     var submit= document.getElementById('submit_btn');
     submit.onclick=function(){
         
-         
-        var names =['name1', 'name2', 'name3', 'name4'];
-        var list='';
-        for (var i=0;i<names.length;i++){
-            list+='<li>'+names[i]+'</li>';
-          
-        }
        
+       
+       
+       var request = new XMLHttpRequest();
         
-       var ul=document.getElementById('namelist') ;
-       ul.innerHTML=list;
-    };  
+        request.onreadystatechange=function(){
+            if (request.readyState==XMLHttpRequest.DONE){
+                  
+                if (request.status==200){
+                    
+                     var names =request.responseText;
+                     names=JSON.parse(names);
+                     var list='';
+                     for (var i=0;i<names.length;i++){
+                            list+='<li>'+names[i]+'</li>';
+          
+                         }//for
+                      
+                        var ul=document.getElementById('namelist') ;
+                         ul.innerHTML=list;
+                 } //request status
+            }  // 200
+       
+          };  
         
+       request.open('GET', 'http://rbhattacharya2001.imad.hasura-app.io/submit-name?name=' +name, true);
+        request.send(null); 
         
+    };
