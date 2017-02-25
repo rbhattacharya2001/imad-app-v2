af74174f6309aa7ca7f7d7cfdcce4d9819c475c1
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-25 17:11:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,12 @@ app.get('/counter',function(req, res) {
 });
 
 
+
+app.get('/test-db',function(req, res) {
+    
+    //
+});
+
 app.get('/article-two',function(req, res) {
  
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
