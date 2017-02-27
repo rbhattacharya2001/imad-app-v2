3c7d0190c27d9dcff60455777b0aa672d5c4a546
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-27 12:40:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool=require('pg').Pool;
+
+var config = {
+    
+    user:'rbhattacharya2001',
+    database:'rbhattacharya2001',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
+
 var app = express();
 app.use(morgan('combined'));
 
