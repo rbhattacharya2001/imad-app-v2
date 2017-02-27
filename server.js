f278acbadef3de7ef52eb5c38a23ac31e51089af
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-27 21:54:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,8 +139,9 @@ app.get('/article-two',function(req, res) {
 });
 
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
    var articleName = req.params.articleName;
+   pool.query("SELECT * FROM article WHERE title=" + req.params.articleName);
    
   res.send(createTemplate(articles[articleName]));
 });
