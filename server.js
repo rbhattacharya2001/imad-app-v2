fe55acd2fb1c9679c3fb99e2d8b93d42f0d982d9
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-02-28 14:25:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,11 +140,19 @@ app.get('/article-two',function(req, res) {
 
 
 app.get('/articles/:articleName', function (req, res) {
-   var articleName = req.params.articleName;
-   pool.query("SELECT * FROM article WHERE title=" + req.params.articleName);
-   
-  res.send(createTemplate(articles[articleName]));
-});
+  // var articleName = req.params.articleName;
+   pool.query("SELECT * FROM article WHERE title=" + req.params.articleName, function(err, result){
+       if (err){
+           res.status(500).send(err.toString());
+       }else{
+            if (result.rows.length===0){
+                res.status(404).send('Artivle not found');
+            }else{
+                var articleData =result.rows[0];
+                res.send(createTemplate(articleData));
+          }
+         }
+}); //selet
 
 
 
