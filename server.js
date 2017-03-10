0b3391ca9fc07840d6c9a4c9ce58658259f21dbc
rbhattacharya2001 <r.bhattacharya2001@gmail.com>
2017-03-10 11:39:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/hash/:input', function (req, res){
 
 app.get('/create-user',function (req, res){
     
-    var salt=crypto.getRandomBytes(128).toString('hex');
+    var salt=crypto.RandomBytes(128).toString('hex');
     
     var dbString=hash(password, salt);
 });
